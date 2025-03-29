4205c01a51165675845d3a18436f8d700f4e3d19
Subhadra2597 <subhadraanju@gmail.com>
2025-03-29 19:54:14 +0530

    added another endpoint

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const port = 3000
 connectdb() //function to connect db
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",credentials:true,methods:["GET","POST","PUT","DELETE"]}))
+app.use(cors({origin:["http://localhost:5173","https://ecommerce-cohort-backend.vercel.app"],
+          credentials:true,
+          methods:["GET","POST","PUT","DELETE"]}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
